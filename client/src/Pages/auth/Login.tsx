74fd5f0abd790e70b6220533dd89ffc4cacfcb6e
ItsMeVishal007 <vishal.codedrill@gmail.com>
2021-10-11 17:48:37 +0530

    adding material ui components

--- client/src/Pages/auth/Login.tsx
+++ client/src/Pages/auth/Login.tsx
@@ -1,16 +1,21 @@
 import React from "react";
-import "../../styles/auth/Login.scss"
+import Typography from "@mui/material/Typography";
+import Input from "../../components/Input";
+import "../../styles/auth/Login.scss";
 
 const Login: React.FC = () => {
   return (
-    <div className='container' >
+    <div className='container'>
       <div className='left-Section'>
-          <div className="wallpaper" />
+        <div className='wallpaper' />
       </div>
       <div className='right-Section'>
         <div className='login-container'>
-          <input placeholder='Email' />
-          <input placeholder='Password' />
+          <Typography variant='h3' component='div' gutterBottom>
+            Login
+          </Typography>
+          <Input label='Email' />
+          <Input label='Password' />
           <button>Submit</button>
         </div>
       </div>
