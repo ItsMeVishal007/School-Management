235ccec256d7209fd1d3acdbde1f2530098dba01
Vishal <yash.codedrill@gmail.com>
2021-10-13 15:26:17 +0530

    define typedef

--- typeDefs/index.ts
+++ typeDefs/index.ts
@@ -1,4 +1,16 @@
 const { ApolloServer, gql } = require('apollo-server');
 const typeDefs = gql`
-
+type Student{
+Name:String
+Email:String
+Age:Number
+ContactNumber:Number
+Address:String
+ParentDetails:String
+RollNumber:String
+}
+type Query{
+Students:[Student]
+}
 `
+module.exports = {typeDefs}
