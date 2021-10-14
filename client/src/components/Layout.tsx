18029d4b6f8c665d61305a8892445dd5ac3aaa61
ItsMeVishal007 <vishal.codedrill@gmail.com>
2021-10-14 18:39:19 +0530

    adding sidebar

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { SideBarContext } from '../context/sidebar';
 import Burgur from '../assets/images/menu.png';
+import cancel from '../assets/images/cancel.png';
 import Button from './Button';
 import '../styles/components/Layout.scss';
 import Text from './Text';
+import SideBar from './SideBar';
 
 interface iLayout {
   children: any;
 }
 
 const Layout: React.FC<iLayout> = ({ children }) => {
+  const { isSideBar, setIsSideBar } = useContext<any>(SideBarContext);
   return (
     <div className="layout">
       <div className="navbar">
         <div className="burgur-menu">
-          <img src={Burgur} alt="burgur" />
+          <img
+            src={isSideBar ? cancel : Burgur}
+            alt="burgur"
+            className={isSideBar && "img"}
+            onClick={() => {
+              setIsSideBar((previousData: boolean) => !previousData);
+            }}
+          />
+
           <Text hType="h3" text="CodeBox..." />
         </div>
         <div className="left-menu">
@@ -23,12 +35,15 @@ const Layout: React.FC<iLayout> = ({ children }) => {
             onClick={() => {
               console.log('entering chat room');
             }}
-            style={{borderColor: 'white'}}
+            style={{ borderColor: 'white' }}
           />
           <div className="userIcon">
             <Text hType="h5" text="Account" />
           </div>
         </div>
+      </div>
+      <div>
+        <SideBar />
       </div>
       {children}
     </div>
