4ff1601c7cc2e6cd05b8f0a4b330724602706c5c
ItsMeVishal007 <vishal.codedrill@gmail.com>
2021-10-15 21:25:48 +0530

    changes

--- client/src/Pages/home/Index.tsx
+++ client/src/Pages/home/Index.tsx
@@ -8,17 +8,17 @@ const cData = [
   {
     title: 'Yash Birthday!',
     detail: 'celebrating yash 44th birthday',
-    image: 'https://source.unsplash.com/2600x900/?forest,wildlife',
+    image: 'https://source.unsplash.com/2600x900/?forest,river',
   },
   {
     title: 'Vishal Birthday!',
     detail: 'celebrating Vishal 44th birthday',
-    image: 'https://source.unsplash.com/2600x900/?forest,wildlife',
+    image: 'https://source.unsplash.com/2600x900/?forest,tree',
   },
   {
     title: 'Pankaj sir Birthday!',
     detail: 'celebrating Pankaj sir 44th birthday',
-    image: 'https://source.unsplash.com/2600x900/?forest,wildlife',
+    image: 'https://source.unsplash.com/2600x900/?forest,animal',
   },
 ];
 
